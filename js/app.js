ca74bcef25bd11bb092ff0451aa3ec05a228d10d
Karl Baker <karl.alec.baker@gmail.com>
2014-05-10 16:37:58 -0700

    Updating to use templates

--- js/app.js
+++ js/app.js
@@ -4,31 +4,67 @@ window.app = angular.module('app', ['ngRoute'])
 
 		$routeProvider
 			.when('/', {
-			    controller: 'videoCtrl',
-			    templateUrl: 'video.html'
+			    controller: 'homeCtrl',
+			    templateUrl: 'home.html'
 			});
 	})
-	.controller('videoCtrl', function($scope) {
+	.controller('homeCtrl', function($scope) {
 		$scope.pitches = [
 			{
-				row: [
-					cols: [
-						{
-							title: 'Project 1',
-							desc: 'A test project',
-							thumbnailUri: ''
-						},
-						{
-							title: 'Project 1',
-							desc: 'A test project',
-							thumbnailUri: ''
-						},
-						{
-							title: 'Project 1',
-							desc: 'A test project',
-							thumbnailUri: ''
-						}
-					]
+				cols: [
+					{
+						title: 'Project 1',
+						desc: 'A test project',
+						thumbnailUri: ''
+					},
+					{
+						title: 'Project 2',
+						desc: 'A test project',
+						thumbnailUri: ''
+					},
+					{
+						title: 'Project 3',
+						desc: 'A test project',
+						thumbnailUri: ''
+					}
+				]
+			},
+			{
+				cols: [
+					{
+						title: 'Project 4',
+						desc: 'A test project',
+						thumbnailUri: ''
+					},
+					{
+						title: 'Project 5',
+						desc: 'A test project',
+						thumbnailUri: ''
+					},
+					{
+						title: 'Project 6',
+						desc: 'A test project',
+						thumbnailUri: ''
+					}
+				]
+			},
+			{
+				cols: [
+					{
+						title: 'Project 7',
+						desc: 'A test project',
+						thumbnailUri: ''
+					},
+					{
+						title: 'Project 8',
+						desc: 'A test project',
+						thumbnailUri: ''
+					},
+					{
+						title: 'Project 9',
+						desc: 'A test project',
+						thumbnailUri: ''
+					}
 				]
 			}
 		];
