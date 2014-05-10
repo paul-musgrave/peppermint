a8bdc2b1ae50982d0b22a13bd86188e59184617e
Karl Baker <karl.alec.baker@gmail.com>
2014-05-10 16:10:30 -0700

    Added angular js to index page, added angular js file to project

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
-window.app = angular.module('app', [])
+window.app = angular.module('app', ['ngRoute'])
+	.config(function($routeProvider, $locationProvider) {
+		$locationProvider.html5Mode(true);
 
-	.controller('indexCtrl', function($scope) {
-
+		$routeProvider
+			.when('/', {
+			    controller: 'videoCtrl',
+			    templateUrl: 'video.html'
+			});
+	})
+	.controller('videoCtrl', function($scope) {
+		$scope.pitches = [
+			{
+				row: [
+					cols: [
+						{
+							title: 'Project 1',
+							desc: 'A test project',
+							thumbnailUri: ''
+						},
+						{
+							title: 'Project 1',
+							desc: 'A test project',
+							thumbnailUri: ''
+						},
+						{
+							title: 'Project 1',
+							desc: 'A test project',
+							thumbnailUri: ''
+						}
+					]
+				]
+			}
+		];
 	});
