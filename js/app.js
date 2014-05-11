15f7ba7af3790e8a25464e0adfe3eaf1d565e409
paulmusgrave <musgrave.paul1@gmail.com>
2014-05-10 18:48:44 -0700

    rating WIP; basic firebase

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-window.app = angular.module('app', ['ngRoute'])
+var FBRef = new Firebase("https://blazing-fire-4190.firebaseio.com/");
+
+window.app = angular.module('app', ['ngRoute', 'firebase'])
 	.config(function($routeProvider, $locationProvider) {
 		$locationProvider.html5Mode(true);
 
@@ -76,15 +78,27 @@ window.app = angular.module('app', ['ngRoute'])
 			}
 		];
 	})
-	.controller('ratingCtrl', function($scope) {
-		$scope.questions = [
-			{
-				text: "Question 1",
-				value: ""
-			},
-			{
-				text: "Question 2",
-				value: ""
-			}
-		];
+	.controller('ratingCtrl', function($scope, $firebase) {
+		$scope.questions = $firebase(FBRef.child('questions'));
+		$scope.comments = $firebase(FBRef.child('comments'));
+		// $scope.questions = [
+		// 	{
+		// 		text: "Question 1",
+		// 		value: ""
+		// 	},
+		// 	{
+		// 		text: "Question 2",
+		// 		value: ""
+		// 	}
+		// ];
+		// $scope.comments = [
+		// 	{
+		// 		username: "user 1",
+		// 		text: "comment 1"
+		// 	},
+		// 	{
+		// 		username: "user 2",
+		// 		text: "comment 2"
+		// 	}
+		// ];
 	});
