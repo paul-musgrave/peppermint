c34db0718e7e76bf3e86bb048fd5e50d88f35c11
paulmusgrave <musgrave.paul1@gmail.com>
2014-05-11 00:55:48 -0700

    added user name and type

--- js/app.js
+++ js/app.js
@@ -4,94 +4,94 @@ window.app = angular.module('app', ['ngRoute', 'firebase'])
     .config(function($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
 
-		$routeProvider
-			.when('/', {
-			    controller: 'homeCtrl',
-			    templateUrl: 'home.html'
-			})
-			.when('/rating', {
-			    controller: 'ratingCtrl',
-			    templateUrl: 'rating.html'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
-	})
-	.controller('homeCtrl', function($scope, $location) {
-		$scope.thumbnailDir = 'images/video-thumbnails/';
-		$scope.pitches = [
-			{
-				cols: [
-					{
-						id: 1,
-						title: 'Project 1',
-						desc: 'A test project',
-						thumbnailUri: '1.jpg'
-					},
-					{
-						id: 2,
-						title: 'Project 2',
-						desc: 'A test project',
-						thumbnailUri: '2.jpg'
-					},
-					{
-						id: 3,
-						title: 'Project 3',
-						desc: 'A test project',
-						thumbnailUri: '3.jpg'
-					}
-				]
-			},
-			{
-				cols: [
-					{
-						id: 4,
-						title: 'Project 4',
-						desc: 'A test project',
-						thumbnailUri: '4.jpg'
-					},
-					{
-						id: 5,
-						title: 'Project 5',
-						desc: 'A test project',
-						thumbnailUri: '5.jpg'
-					},
-					{
-						id: 6,
-						title: 'Project 6',
-						desc: 'A test project',
-						thumbnailUri: '6.jpg'
-					}
-				]
-			},
-			{
-				cols: [
-					{
-						id: 7,
-						title: 'Project 7',
-						desc: 'A test project',
-						thumbnailUri: '7.jpg'
-					},
-					{
-						id: 8,
-						title: 'Project 8',
-						desc: 'A test project',
-						thumbnailUri: '8.jpg'
-					},
-					{
-						id: 9,
-						title: 'Project 9',
-						desc: 'A test project',
-						thumbnailUri: '9.jpg'
-					}
-				]
-			}
-		];
+        $routeProvider
+            .when('/', {
+                controller: 'homeCtrl',
+                templateUrl: 'home.html'
+            })
+            .when('/rating', {
+                controller: 'ratingCtrl',
+                templateUrl: 'rating.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .controller('homeCtrl', function($scope, $location) {
+        $scope.thumbnailDir = 'images/video-thumbnails/';
+        $scope.pitches = [
+            {
+                cols: [
+                    {
+                        id: 1,
+                        title: 'Project 1',
+                        desc: 'A test project',
+                        thumbnailUri: '1.jpg'
+                    },
+                    {
+                        id: 2,
+                        title: 'Project 2',
+                        desc: 'A test project',
+                        thumbnailUri: '2.jpg'
+                    },
+                    {
+                        id: 3,
+                        title: 'Project 3',
+                        desc: 'A test project',
+                        thumbnailUri: '3.jpg'
+                    }
+                ]
+            },
+            {
+                cols: [
+                    {
+                        id: 4,
+                        title: 'Project 4',
+                        desc: 'A test project',
+                        thumbnailUri: '4.jpg'
+                    },
+                    {
+                        id: 5,
+                        title: 'Project 5',
+                        desc: 'A test project',
+                        thumbnailUri: '5.jpg'
+                    },
+                    {
+                        id: 6,
+                        title: 'Project 6',
+                        desc: 'A test project',
+                        thumbnailUri: '6.jpg'
+                    }
+                ]
+            },
+            {
+                cols: [
+                    {
+                        id: 7,
+                        title: 'Project 7',
+                        desc: 'A test project',
+                        thumbnailUri: '7.jpg'
+                    },
+                    {
+                        id: 8,
+                        title: 'Project 8',
+                        desc: 'A test project',
+                        thumbnailUri: '8.jpg'
+                    },
+                    {
+                        id: 9,
+                        title: 'Project 9',
+                        desc: 'A test project',
+                        thumbnailUri: '9.jpg'
+                    }
+                ]
+            }
+        ];
 
-		$scope.go = function() {
-			$location.path('/rating/');
-		};
-	})
+        $scope.go = function() {
+            $location.path('/rating/');
+        };
+    })
     .controller('ratingCtrl', function($scope, $firebase) {
         $scope.user_id = uuid(); //#
 
@@ -101,6 +101,17 @@ window.app = angular.module('app', ['ngRoute', 'firebase'])
 
         $scope.questions = $firebase(questionsRef);
         $scope.comments = $firebase(videoRef.child('comments'));
+
+        $scope.$watch('userName', function(){
+            if($scope.userName){
+                responseRef.child($scope.user_id+'/userName').set($scope.userName);
+            }
+        });
+        $scope.$watch('userType', function(){
+            if($scope.userType){
+                responseRef.child($scope.user_id+'/userType').set($scope.userType);
+            }
+        });
 
 
         questionsRef.on('child_added', function(ss){
